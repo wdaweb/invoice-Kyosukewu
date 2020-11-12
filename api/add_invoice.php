433471380ca7312e9ef1fa498d57c38d68e95170
Kyosukewu <kyosukewu2020@gmail.com>
2020-11-12 16:24:57 +0800

    編輯base

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -2,7 +2,7 @@
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
 
-include_once("base.php");// once 只要一份
+include_once("../base.php");// once 只要一份
 // foreach($_POST as $key =>$value){
 //     $tmp[]=$key;
 // }
