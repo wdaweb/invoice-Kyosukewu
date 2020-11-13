0e4131b1d8c016049e5f109ed9a0d3c7ecf09ab6
Kyosukewu <kyosukewu2020@gmail.com>
2020-11-13 08:51:59 +0800

    調整版面結構     modified:   api/add_invoice.php -> 把儲存後的導向改成檢視發票清單(do=invoice_list) 	modified:   index.php -> 增加共用的內容區塊(功能選項) 	modified:   invoice_list.php -> 去除重複html碼 	modified:   main.php -> 把共用的區塊移到index.php,留下新增發票的清單

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -19,5 +19,5 @@ echo $sql;
 $pdo->exec($sql);
 
 echo "新增完成";
-header("location:../index.php");
+header("location:../index.php?do=invoice_list");
 ?>
--- index.php
+++ index.php
@@ -1,26 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
-<link rel="stylesheet" href="css/reset.css">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
-<title>統一發票紀錄及對獎系統</title>
-<style type="text/css">
-    
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/reset.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+    <title>統一發票紀錄及對獎系統</title>
+    <style type="text/css">
+
+    </style>
 </head>
+
 <body>
-    <?php
+    <div class="h3 text-center">統一發票紀錄與對獎</div>
+    <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
+        <?php
+        $month = [
+            1 => "1,2月",
+            2 => "3,4月",
+            3 => "5,6月",
+            4 => "7,8月",
+            5 => "9,10月",
+            6 => "11,12月",
+        ];
+
+        $m = ceil(date("m") / 2);
+
+        ?>
+        <div class="text-center"><?= $month[$m]; ?></div>
+        <div class="text-center">
+            <a href="?do=invoice_list">當期發票</a>
+        </div>
+        <div class="text-center">
+            <a href="">對獎</a>
+        </div>
+        <div class="text-center">
+            <a href="">輸入獎號</a>
+        </div>
+        <div class="text-center">
+            <a href="index.php">回首頁</a>
+        </div>
+    </div>
+    <div class="col-8 d-flex p-3 mx-auto border">
+        <?php
 
 
-if(isset($_GET['do'])){
-    $file=$_GET['do'].".php";
-    include $file;    
-}else{
-    include "main.php";
-}
-    ?>
+        if (isset($_GET['do'])) {
+            $file = $_GET['do'] . ".php";
+            include $file;
+        } else {
+            include "main.php";
+        }
+        ?>
+    </div>
 </body>
+
 </html>
