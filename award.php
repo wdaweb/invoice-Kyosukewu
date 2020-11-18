3f09d758c5497e79455dc2beda45ae50173394db
Kyosukewu <kyosukewu2020@gmail.com>
2020-11-18 14:48:13 +0800

    增加最終獎項判斷及沒中獎時的訊息

--- award.php
+++ award.php
@@ -25,37 +25,47 @@ $awards=$pdo->query("select * from award_numbers where year='$year' && period='$
 // echo "<pre>";
 // print_r($awards);
 // echo "</pre>";
-
+$all_res=-1;
 foreach($awards as $award){
     switch($award['type']){
         case 1://特別號
             if($award['number']==$number){
                 echo "號碼：".$number."<br>中大獎啦！中了特別獎啦~";
+                $all_res=1;
             }
         break;
         case 2://特獎
             if($award['number']==$number){
                 echo "號碼：".$number."<br>中大獎啦！中了特獎啦~";
+                $all_res=1;
             }
         break;
         case 3://頭獎
+            $res=-1;
             for($i=5;$i>=0;$i--){//從尾數開始對
                 $target=mb_substr($award['number'],$i,(8-$i),'utf8');
                 $mynumber=mb_substr($number,$i,(8-$i),'utf8');
 
             if($target==$mynumber){
-                echo "號碼：".$number."<br>中獎啦！中了{$awardStr[$i]}獎啦~<br>";
+                // echo "號碼：".$number."<br>中獎啦！中了{$awardStr[$i]}獎啦~<br>";
+                $res=$i;
             }else{
                 break;//continue;
             }
+        }
+        if($res!=-1){//判斷最終的獎項
+        echo "號碼：".$number."<br>中獎啦！中了{$awardStr[$res]}獎啦~<br>";
+        $all_res=1;
         }
         break;
         case 4://增開六獎
             if($award['number']==mb_substr($number,5,3,'utf8')){
                 echo "號碼：".$number."<br>中獎啦！中了增開陸獎啦~<br>";
             }
+            $all_res=1;
         break;
     }
 }
-
-?>
+if($all_res==-1){
+    echo "可惜不是你,再接再厲吧";
+}
